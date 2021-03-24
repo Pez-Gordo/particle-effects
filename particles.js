5c91b9c5c2ee285f1cc2b7bcb9a85bde6694d7e5
pez <emvenegas@gmail.com>
2021-03-24 00:38:36 +0000

    exercise finished

--- particles.js
+++ particles.js
@@ -71,7 +71,7 @@ class Particle {
 function init() {
     particlesArray = []
     let numberOfParticles = (canvas.height * canvas.width) / 9000
-    for (let i = 0; i< numberOfParticles; i++) {
+    for (let i = 0; i < numberOfParticles * 2; i++) {
         let size = (Math.random() * 5) + 1
         let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2)
         let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2)
@@ -83,6 +83,26 @@ function init() {
     }
 }
 
+// check if particles are close enough
+function connect() {
+    let opacityValue = 1
+    for (let a= 0; a < particlesArray.length; a++) {
+        for (let b = 0; b < particlesArray.length; b++) {
+            let distance = ((particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x))
+             + ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y)) 
+             if (distance < (canvas.width / 7) * (canvas.height / 7)) {
+                 opacityValue = 1 - (distance / 20000)
+                 ctx.strokeStyle = 'rgba(140, 85, 31,' + opacityValue + ')'
+                 ctx.lineWidth = 1
+                 ctx.beginPath()
+                 ctx.moveTo(particlesArray[a].x, particlesArray[a].y)
+                 ctx.lineTo(particlesArray[b].x, particlesArray[b].y)
+                 ctx.stroke()
+             }
+        }
+    }
+}
+
 //animation loop
 function animate() {
     requestAnimationFrame(animate)
@@ -91,6 +111,23 @@ function animate() {
     for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update()
     }
+    connect()
 }
+
+// resize event
+window.addEventListener('resize', function() {
+    canvas.width = innerWidth
+    canvas.height = innerHeight
+    mouse.radius = ((canvas.height / 80) * (canvas.height / 80))
+    init()
+})
+
+// mouse out event
+window.addEventListener('mouseout', function() {
+    mouse.x = undefined
+    mouse.x = undefined
+
+})
+
 init()
 animate()
